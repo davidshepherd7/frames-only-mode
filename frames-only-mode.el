043df384129f9fabe21484490b6aa1667492e4dc
David Shepherd <davidshepherd7@gmail.com>
2014-12-04 09:23:23 +0000

    More comments

--- frames-only-mode.el
+++ frames-only-mode.el
@@ -40,12 +40,15 @@
 (set 'org-agenda-window-setup 'other-frame)
 (set 'org-src-window-setup 'other-frame)
 
+;; Make calendar do something more sensible with its window/frame layout.
 (defadvice calendar (around disable-pop-up-frames activate)
   "Disable pop-up-frames while this is going on, otherwise we get
 extra useless frames."
   (let ((pop-up-frames 'nil))
     ad-do-it))
 
+;; Key bind to close sub-windows (e.g. as created by re-builder or
+;; calendar) with the same key as abort-recursive-edit.
 (defun super-abort-recursive-edit ()
   "kill any sub-windows and abort recursive edit."
   (interactive)
@@ -56,7 +59,6 @@ extra useless frames."
   (delete-other-windows)
 
   (abort-recursive-edit))
-
 (global-set-key [remap abort-recursive-edit] #'super-abort-recursive-edit)
 
 
@@ -65,13 +67,14 @@ extra useless frames."
 ;; frames
 (set 'frame-auto-hide-function 'delete-frame)
 
+;; Hack to make other things play nice by killing the frame when certain
+;; buffers are closed.
 (defun kill-frame-if-current-buffer-matches ()
   "Kill frames as well when certain buffers are closed, helps stop some
   packages spamming frames."
   (interactive)
   (if (member (buffer-name) kill-frame-when-buffer-killed-buffer-list)
       (delete-frame)))
-
 (add-hook 'kill-buffer-hook 'kill-frame-if-current-buffer-matches)
 
 
