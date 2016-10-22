20629885a7c4a454a63f4a4b596563bbd4eeff5c
David Shepherd <davidshepherd7@gmail.com>
2016-10-22 12:22:35 +0100

    Convert to global minor mode

--- frames-only-mode.el
+++ frames-only-mode.el
@@ -104,100 +104,84 @@ To disable completion popups entirely use the variable
 
 
 
-;;; Set up
+(define-minor-mode frames-only-mode
+  "Use frames instead of emacs windows."
+  :global t
 
-;; Make new frames instead of new windows, the main setting
-(set 'pop-up-frames 'graphic-only)
-
-
-;; Focus follows mouse (for emacs windows) off to prevent crazy things
-;; happening when I click on e.g. compilation error links. Would do nothing
-;; interesting anyway if everything is working because there are no windows
-;; within frames.
-(set 'mouse-autoselect-window nil)
-(set 'focus-follows-mouse nil)
+  ;; Make new frames instead of new windows, the main setting
+  (set 'pop-up-frames 'graphic-only)
 
 
-;; Disable in some functions as specified by customisation
-(mapc (lambda (fun) (advice-add fun :around #'frames-only-mode-advice-use-windows))
-      frames-only-mode-use-window-functions)
+  ;; Focus follows mouse (for emacs windows) off to prevent crazy things
+  ;; happening when I click on e.g. compilation error links. Would do nothing
+  ;; interesting anyway if everything is working because there are no windows
+  ;; within frames.
+  (set 'mouse-autoselect-window nil)
+  (set 'focus-follows-mouse nil)
 
 
-;; Key bind to close sub-windows (e.g. as created by re-builder or
-;; calendar) with the same key as abort-recursive-edit.
-(global-set-key [remap abort-recursive-edit] #'super-abort-recursive-edit)
+  ;; Disable in some functions as specified by customisation
+  (mapc (lambda (fun) (advice-add fun :around #'frames-only-mode-advice-use-windows))
+        frames-only-mode-use-window-functions)
 
 
-;; kill frames when a buffer is buried, makes most things play nice with
-;; frames
-(set 'frame-auto-hide-function 'delete-frame)
-
-;; Hacks to make other things play nice by killing the frame when certain
-;; buffers are closed.
-(add-hook 'kill-buffer-hook 'kill-frame-if-current-buffer-matches)
+  ;; Key bind to close sub-windows (e.g. as created by re-builder or
+  ;; calendar) with the same key as abort-recursive-edit.
+  (global-set-key [remap abort-recursive-edit] #'super-abort-recursive-edit)
 
 
-
+  ;; kill frames when a buffer is buried, makes most things play nice with
+  ;; frames
+  (set 'frame-auto-hide-function 'delete-frame)
 
-;;; gud
-
-;; gdb (gud) does things with windows by default, this stops some of it:
-(set 'gdb-use-separate-io-buffer nil)
-(set 'gdb-many-windows nil)
-
-
-
-
-;;; org-mode
-
-;; org windows: Use frames not emacs windows
-(set 'org-agenda-window-setup 'other-frame)
-(set 'org-src-window-setup 'other-frame)
+  ;; Hacks to make other things play nice by killing the frame when certain
+  ;; buffers are closed.
+  (add-hook 'kill-buffer-hook 'kill-frame-if-current-buffer-matches)
 
 
-
+  ;; gdb (gud) does things with windows by default, this stops some of it:
+  (set 'gdb-use-separate-io-buffer nil)
+  (set 'gdb-many-windows nil)
 
-;;; magit
 
-(when (require 'magit nil 'noerror)
+  ;; org windows: Use frames not emacs windows
+  (set 'org-agenda-window-setup 'other-frame)
+  (set 'org-src-window-setup 'other-frame)
 
-  ;; Use the current frame/window to enter the magit commit message
-  (set 'magit-server-window-for-commit nil)
 
-  ;; Don't auto popup a magit diff buffer when commiting, can still get it
-  ;; if needed with C-c C-d. The variable name for this changed in magit
-  ;; version 2.30, so for now we will set both variables (28/11/2015).
-  ;; TODO: remove in a year or two.
-  (if (boundp 'magit-commit-show-diff)
-      (set 'magit-commit-show-diff nil)
-    (set 'magit-diff-auto-show nil))
+  (when (require 'magit nil 'noerror)
+
+    ;; Use the current frame/window to enter the magit commit message
+    (set 'magit-server-window-for-commit nil)
+
+    ;; Don't auto popup a magit diff buffer when commiting, can still get it
+    ;; if needed with C-c C-d. The variable name for this changed in magit
+    ;; version 2.30, so for now we will set both variables (28/11/2015).
+    ;; TODO: remove in a year or two.
+    (if (boundp 'magit-commit-show-diff)
+        (set 'magit-commit-show-diff nil)
+      (set 'magit-diff-auto-show nil))
+
+    )
+
+
+  ;; Advise completion popup functions to use windows instead of frames if
+  ;; the custom setting is true.
+  (advice-add 'minibuffer-completion-help :around #'advice-use-windows-for-completion)
+  (advice-add 'ido-completion-help :around #'advice-use-windows-for-completion)
+
+  ;; Make sure completions buffer is buried after we are done with the minibuffer
+  (add-hook 'minibuffer-exit-hook (lambda () (when (get-buffer "*Completions*")
+                                          (bury-buffer "*Completions*"))))
+
+
+
+  ;; Use a single frame for ediff (without this you end up with an entire
+  ;; frame for the control buffer, this doesn't work well at all with tiling
+  ;; window managers).
+  (set 'ediff-window-setup-function 'ediff-setup-windows-plain)
 
   )
-
-
-
-
-;;; Completion
-
-;; Advise completion popup functions to use windows instead of frames if
-;; the custom setting is true.
-(advice-add 'minibuffer-completion-help :around #'advice-use-windows-for-completion)
-(advice-add 'ido-completion-help :around #'advice-use-windows-for-completion)
-
-;; Make sure completions buffer is buried after we are done with the minibuffer
-(add-hook 'minibuffer-exit-hook (lambda () (when (get-buffer "*Completions*")
-                                        (bury-buffer "*Completions*"))))
-
-
-
-
-;;; ediff
-
-;; Use a single frame for ediff (without this you end up with an entire
-;; frame for the control buffer, this doesn't work well at all with tiling
-;; window managers).
-(set 'ediff-window-setup-function 'ediff-setup-windows-plain)
-
 
 
 (provide 'frames-only-mode)
