d316c95392e4e1753615a8deac15540742ef7702
David Shepherd <davidshepherd7@gmail.com>
2016-10-22 12:16:46 +0100

    Separate helper functions and actual actions

--- frames-only-mode.el
+++ frames-only-mode.el
@@ -57,7 +57,54 @@ To disable completion popups entirely use the variable
 
 
 
-;;; Basics
+;;; Helper functions
+
+(defun frames-only-mode-advice-use-windows (fun &rest args)
+  "Create new emacs windows instead of frames within this function"
+  (let ((pop-up-frames nil))
+    (apply fun args)))
+
+
+(defun super-abort-recursive-edit ()
+  "kill any sub-windows and abort recursive edit."
+  (interactive)
+
+  ;; Biggest window is probably the "main" one, select it and delete the
+  ;; rest.
+  (select-window (get-largest-window 't 'nil 'nil))
+  (delete-other-windows)
+
+  (abort-recursive-edit))
+
+
+(defun kill-frame-if-current-buffer-matches ()
+  "Kill frames as well when certain buffers are closed (but only
+  if there is only a single window in the frame), helps stop some
+  packages spamming frames."
+  (when (and (one-window-p)
+             (member (buffer-name) frames-only-mode-kill-frame-when-buffer-killed-buffer-list))
+    (delete-frame)))
+
+
+(defun advice-use-windows-for-completion (orig-fun &rest args)
+  (let ((pop-up-frames (not frames-only-mode-use-windows-for-completion))
+        (split-width-threshold 9999))
+    (apply orig-fun args)))
+
+
+(defadvice bury-buffer (around kill-frame-if-current-buffer-matches activate)
+  "Kill the frame when burying certain buffers (but only if there
+  is only a single window in the frame)."
+  (let ((buffer-to-bury (buffer-name)))
+    ad-do-it
+    (when (and (one-window-p)
+               (member buffer-to-bury frames-only-mode-kill-frame-when-buffer-killed-buffer-list))
+      (delete-frame))))
+
+
+
+
+;;; Set up
 
 ;; Make new frames instead of new windows, the main setting
 (set 'pop-up-frames 'graphic-only)
@@ -72,27 +119,12 @@ To disable completion popups entirely use the variable
 
 
 ;; Disable in some functions as specified by customisation
-(defun frames-only-mode-advice-use-windows (fun &rest args)
-  "Create new emacs windows instead of frames within this function"
-  (let ((pop-up-frames nil))
-    (apply fun args)))
-
 (mapc (lambda (fun) (advice-add fun :around #'frames-only-mode-advice-use-windows))
       frames-only-mode-use-window-functions)
 
 
 ;; Key bind to close sub-windows (e.g. as created by re-builder or
 ;; calendar) with the same key as abort-recursive-edit.
-(defun super-abort-recursive-edit ()
-  "kill any sub-windows and abort recursive edit."
-  (interactive)
-
-  ;; Biggest window is probably the "main" one, select it and delete the
-  ;; rest.
-  (select-window (get-largest-window 't 'nil 'nil))
-  (delete-other-windows)
-
-  (abort-recursive-edit))
 (global-set-key [remap abort-recursive-edit] #'super-abort-recursive-edit)
 
 
@@ -102,25 +134,7 @@ To disable completion popups entirely use the variable
 
 ;; Hacks to make other things play nice by killing the frame when certain
 ;; buffers are closed.
-(defun kill-frame-if-current-buffer-matches ()
-  "Kill frames as well when certain buffers are closed (but only
-  if there is only a single window in the frame), helps stop some
-  packages spamming frames."
-  (when (and (one-window-p)
-             (member (buffer-name) frames-only-mode-kill-frame-when-buffer-killed-buffer-list))
-    (delete-frame)))
-
 (add-hook 'kill-buffer-hook 'kill-frame-if-current-buffer-matches)
-
-
-(defadvice bury-buffer (around kill-frame-if-current-buffer-matches activate)
-  "Kill the frame when burying certain buffers (but only if there
-  is only a single window in the frame)."
-  (let ((buffer-to-bury (buffer-name)))
-    ad-do-it
-    (when (and (one-window-p)
-               (member buffer-to-bury frames-only-mode-kill-frame-when-buffer-killed-buffer-list))
-      (delete-frame))))
 
 
 
@@ -167,10 +181,6 @@ To disable completion popups entirely use the variable
 
 ;; Advise completion popup functions to use windows instead of frames if
 ;; the custom setting is true.
-(defun advice-use-windows-for-completion (orig-fun &rest args)
-  (let ((pop-up-frames (not frames-only-mode-use-windows-for-completion))
-        (split-width-threshold 9999))
-    (apply orig-fun args)))
 (advice-add 'minibuffer-completion-help :around #'advice-use-windows-for-completion)
 (advice-add 'ido-completion-help :around #'advice-use-windows-for-completion)
 
